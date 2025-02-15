638dd48da860e76d4f4dc44c1d6f449af43e0bff
Sadriddin21122024 <nasriddinovbiloliddin@gmail.com>
2025-02-15 21:16:52 +0500

    contack ejs qoshdim

--- controller/render.controller.js
+++ controller/render.controller.js
@@ -13,5 +13,11 @@ let renderAbout = (req, res, next) => {
     title: "Biz haqimizda",
   });
 };
+let renderContact = (req, res, next) => {
+  let data = courseService.getAllCourses();
+  res.render("contact", { 
+    title: "Biz haqimizda",
+  });
+};
 
 module.exports = { renderIndex, renderAbout };
--- routes/render.route.js
+++ routes/render.route.js
@@ -4,5 +4,6 @@ const renderController = require("../controller/render.controller");
 router.get("/", renderController.renderIndex);
 router.get("/index", renderController.renderIndex);
 router.get("/about", renderController.renderAbout);
+router.get("/contact", renderController.renderAbout);
 
 module.exports = router;
